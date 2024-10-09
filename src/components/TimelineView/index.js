8488051e06ad475a93df61f85cf6eac2b1e0c948
reddykarunakar <reddykarunakar.7674@gmail.com>
2024-10-09 08:00:26 +0000

    solution

---
+++ src/components/TimelineView/index.js
@@ -0,0 +1,46 @@
+// Write your code here
+import {Chrono} from 'react-chrono'
+
+import ProjectTimelineCard from '../ProjectTimelineCard'
+import CourseTimelineCard from '../CourseTimelineCard'
+
+import {
+  TimelineContainer,
+  ResponsiveContainer,
+  HeaderContainer,
+  Heading,
+  CCBPHeading,
+} from './styledComponents'
+
+const TimelineView = props => {
+  const {timelineItemsList} = props
+
+  const renderTimelineCard = item => {
+    if (item.category === 'PROJECT') {
+      return <ProjectTimelineCard key={item.id} projectDetails={item} />
+    }
+    return <CourseTimelineCard key={item.id} courseDetails={item} />
+  }
+
+  return (
+    <TimelineContainer>
+      <ResponsiveContainer>
+        <HeaderContainer>
+          <Heading>
+            MY JOURNEY OF <br />
+            <CCBPHeading>CCBP 4.0</CCBPHeading>
+            <Chrono
+              theme={{secondary: 'white'}}
+              items={timelineItemsList}
+              mode="VERTICAL_ALTERNATING"
+            >
+              {timelineItemsList.map(eachItem => renderTimelineCard(eachItem))}
+            </Chrono>
+          </Heading>
+        </HeaderContainer>
+      </ResponsiveContainer>
+    </TimelineContainer>
+  )
+}
+
+export default TimelineView
